21da327460714658a5d1febcab508bfdf7ae9945
Naveen Malik <nmalik@redhat.com>
2015-02-22 11:50:55 -0500

    Updated scraping script to support ctrl+c and command line args for id range

--- scrape-wake-gov-realestate.py
+++ scrape-wake-gov-realestate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from threading import Thread
+import sys
 import logging
 import os.path
 import math
@@ -8,16 +9,20 @@ import urllib2
 # http://services.wakegov.com/realestate/Account.asp?id=0409178
 
 threads = []
+running = True
 
 def create_thread(id_start, id_end):
-    logging.debug('Thread started: %s, %s', id_start, id_end)
+    logging.debug('create_thread(%s, %s)', id_start, id_end)
     t = Thread(target=do_scrape, args=(id_start, id_end, ) )
     threads.append(t)
 
 # Define a function for the thread
 def do_scrape(id_start, id_end):
-    #print "scrape[{}, {}]".format(int(id_start), int(id_end))
-    for x in range(int(id_start), int(id_end)):
+    logging.debug('do_scrape(%s, %s)', id_start, id_end)
+    # end of range is not inclusive
+    for x in range(int(id_start), int(id_end) + 1):
+        if not running:
+            break
         filename = "data/{}".format(x)
         if os.path.isfile(filename):
             logging.debug('%s: data exists', x)
@@ -35,18 +40,29 @@ def do_scrape(id_start, id_end):
             except urllib2.URLError as e:
                 logging.error('%s: URLError - %s', x, e.reason)
 
-# setup logging
-logging.basicConfig(format='time="%(asctime)s" level="%(levelname)s" message="%(message)s"', level=logging.DEBUG, filename='scrape.log')
-logging.info('START')
+# create data and target dir if it doesn't exist
+if not os.path.exists("target"):
+    os.makedirs("target")
 
-# create data dir if it doesn't exist
 if not os.path.exists("data"):
     os.makedirs("data")
 
-thread_count = 100
+# setup logging
+logging.basicConfig(format='time="%(asctime)s" level="%(levelname)s" message="%(message)s"', level=logging.DEBUG, filename='target/scrape.log')
+logging.info('START')
+
+# setup range
+# note, argv[0] is program name
 # first index is not inclusive
-#thread_ranges = [0, 409178]
-thread_ranges = [100000, 200000]
+print sys.argv
+if len(sys.argv) == 3:
+    thread_ranges = [int(sys.argv[1]), int(sys.argv[2])]
+else:
+    thread_ranges = [0, 400000]
+
+print "Using ID range: [{}, {}]".format(thread_ranges[0], thread_ranges[1])
+
+thread_count = 10
 total = thread_ranges[1] - thread_ranges[0]
 thread_range = math.floor(total / thread_count)
 
@@ -60,8 +76,16 @@ if max_range < thread_ranges[1]:
 
 [x.start() for x in threads]
 
-for x in threads:
-    x.join()
-    logging.info('THREAD CLOSED: %s', x.ident)
+while len(threads) > 0:
+    try:
+        for x in threads:
+            x.join(100)
+            if not x.is_alive():
+                logging.info('THREAD CLOSED: %s', x.ident)
+                threads.remove(x)
+    except KeyboardInterrupt:
+        print "Program will exist in a moment..."
+        running = False
+        break
 
 logging.info('END')
